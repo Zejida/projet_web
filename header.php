b46331918c8a1769055107dfd1c63df754f143bf
COINON Cedric <cedric.coinon@etud.u-picardie.fr>
2017-02-01 22:55:19 +0100

    navbar.php devient header.php + ajout /img/banner/ban1.png

---
+++ header.php
@@ -0,0 +1,36 @@
+<div class="grid grid-pad">
+    <div class="col-1-1">
+        <div class="content">
+        <a href="">
+        	<img src="img\banner\ban1.png" alt="Baniére" title="La Halle au Frais"/>
+        </a>
+        </div>
+    </div>
+</div>
+<div class="grid grid-pad">
+	<div class="col-1-5" id="onglet_actif">
+		<div class="content">
+			<li><a href="index.php" title="Accueil">Accueil</a></li>
+		</div>
+	</div>
+	<div class="col-1-5 menu">
+		<div class="content">
+		<li><a href="#" title="Qui sommes-nous ?">Qui sommes-nous ?</a></li>
+		</div>
+	</div>
+	<div class="col-1-5 menu">
+		<div class="content">
+		<li><a href="#" title="Actualités">Actualités</a></li>
+		</div>
+	</div>
+	<div class="col-1-5 menu">
+		<div class="content">
+		<li><a href="#" title="Producteurs">Producteurs	</a></li>
+		</div>
+	</div>
+	<div class="col-1-5 menu">
+		<div class="content">
+		<li><a href="#" title="Contacts">Contacts</a></li>
+		</div>
+	</div>
+</div>
