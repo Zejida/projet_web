1d5159fbbf75b9bcac021bc8025efb9b27e5916d
COINON Cedric <cedric.coinon@etud.u-picardie.fr>
2017-02-01 23:09:30 +0100

    MAJ index.php - inclusion du header

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="icon" type="image/png" href="img\favicon\fav1.png" />
+		<link rel="icon" type="image/png" href="img\favicon\fav1.png"/>
+		<link rel="stylesheet" href="assets/simplegrid.css"/>
+		<link rel="stylesheet" href="assets/reset-css.css"/>
 		<title>La Halle au Frais</title>
 		<meta charset="UTF-8" />
+		<meta name="description" content="La Halle au Frais">
+		<meta content="true" name="HandheldFriendly">
 	</head>
 	<body>
 		<header>
+		<?php include ("header.php"); ?>
 		</header>
-		
 		<main>
 			<article>
     				<header>
