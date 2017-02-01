7b889a460283441dbc2943d6f3db6a61fb9333a9
COINON Cedric <cedric.coinon@etud.u-picardie.fr>
2017-02-01 17:25:08 +0100

    creation repertoire img + ajout favicon sur index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="icon" type="image/png" href="img\favicon\fav1.png" />
+		<title>La Halle au Frais</title>
+		<meta charset="UTF-8" />
+	</head>
+	<body>
+		<header>
+		</header>
+		
+		<main>
+			<article>
+    				<header>
+    				</header>
+    				<section>
+        			<p>...</p>
+    				</section>
+    				<footer>
+   					</footer>
+			</article>
+		</main>
+		<footer>
+		</footer>
+	</body>
+</html>
